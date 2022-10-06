9d0a19e5099b81d31e3a756c5ce271d8d34a8e28
Adnan <adnanhani552@gmail.com>
2022-10-07 00:14:44 +0400

    make it better

--- animals/asgi.py
+++ animals/asgi.py
@@ -13,8 +13,8 @@ import letter.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animals.settings')
 
 application = ProtocolTypeRouter({
-
-    'websocket': AuthMiddlewareStack(
+    'http':get_asgi_application(),
+    'websocket':AuthMiddlewareStack(
         URLRouter(
             animal.routing.websocket_urlpatterns+
             gameadmin.routing.websocket_urlpatterns+
