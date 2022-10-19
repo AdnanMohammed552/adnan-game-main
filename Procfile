6c79acc73b7e72bcd7cda1bd531069facdb7ffb1
AdnanMohammed552 <115033110+AdnanMohammed552@users.noreply.github.com>
2022-10-19 22:38:34 +0400

    make it better

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn animals.wsgi
-web: daphne animals.asgi:application --port $PORT --bind 0.0.0.0 -v2
--- gameadmin/routing.py
+++ gameadmin/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns= [
-    path('xws/<int:room_code>/admin/' , consumers.gameConsumer.as_asgi()),
+    path('ws/<int:room_code>/admin/' , consumers.gameConsumer.as_asgi()),
 ]
