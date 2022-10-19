6e4ffc2bb5e49448207d0037fc44ea7ff17aea1c
AdnanMohammed552 <115033110+AdnanMohammed552@users.noreply.github.com>
2022-10-19 22:34:23 +0400

    make it better

--- gameadmin/routing.py
+++ gameadmin/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns= [
-    path('ws/<int:room_code>/admin/' , consumers.gameConsumer.as_asgi()),
+    path('xws/<int:room_code>/admin/' , consumers.gameConsumer.as_asgi()),
 ]
