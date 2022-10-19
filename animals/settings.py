66d0e5dcb14529f5d8bcbedb5a26bb22c5bb7c9d
AdnanMohammed552 <115033110+AdnanMohammed552@users.noreply.github.com>
2022-10-19 23:07:10 +0400

    make it better

--- animals/settings.py
+++ animals/settings.py
@@ -82,7 +82,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+             "hosts": [("ec2-44-207-16-250.compute-1.amazonaws.com", 12690)],
         },
     },
 }
