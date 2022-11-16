b8e2bbe8a6f2faa1fcd2fe28cb88f8f3e51ea582
ADNAN MOHAMMED <adnanhani552@gmail.com>
2022-11-16 19:55:57 +0400

    make it better

--- animal/views.py
+++ animal/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User 
 
 x= ["أ","ب","ت","ث","ج","ح","خ","د","ذ","ر","ز","س","ش","ص","ض","ط","ع","غ","ف","ق","ك","ل","م","ن","ه","و","ي"]
-the_letter = random.choice(x)
+the_letter = 'ب'
 print('this is letterc from views',the_letter)
 def roomentering(request):
     return render(request,'room.html')
@@ -46,7 +46,7 @@ def room(request,room_code):
     #print('my data is::',t)
     if str(room_code) not in t :
         print('yse is none')
-        models.summeryOfLetter.objects.create(all_letter_as="['أ', 'ب', 'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي']",room=room_code)
+        models.summeryOfLetter.objects.create(all_letter_as="['أ','ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي']",room=room_code)
         
         return render(request,"game.html", {'username':username , 'room_code' : room_code ,'info':info , 'the_letter':the_letter ,})
 
@@ -60,7 +60,7 @@ def room(request,room_code):
 def room_admin(request,room_code):
 
     info = adnan_test11.objects.filter(room=room_code)
-    models.summeryOfLetter.objects.create(all_letter_as="['أ', 'ب', 'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي']",room=room_code)
+    models.summeryOfLetter.objects.create(all_letter_as="['أ','ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي']",room=room_code)
 
     
     return render(request, "startroom.html", {'info': info , 'room_code':room_code ,'the_letter':the_letter})
@@ -95,7 +95,7 @@ def joinnow(request,room_code):
     if True:
         print('yse is none')
         print('vies room codeis',room_code)
-        models.summeryOfLetter.objects.create(all_letter_as="['أ', 'ب', 'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي']",room=room_code)
+        models.summeryOfLetter.objects.create(all_letter_as="['أ','ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي']",room=room_code)
         
         return render(request,"game.html", {'username':username , 'room_code' : room_code ,'info':info , 'the_letter':the_letter ,})
 
