3b88711c34a132473266f0594d30e935ce3d4517
ADNAN MOHAMMED <adnanhani552@gmail.com>
2022-11-28 23:36:09 +0400

    make it better

--- gameadmin/models.py
+++ gameadmin/models.py
@@ -7,7 +7,7 @@ class startingroom(models.Model):
     started = models.BooleanField()
     end = models.BooleanField()
     time = models.DateTimeField(default=timezone.now)
-    admin=models.CharField(max_length=150)
+    admin=models.CharField(max_length=150,default='a')
 
     def __str__(self):
         return self.room
