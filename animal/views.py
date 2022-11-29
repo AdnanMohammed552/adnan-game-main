1d934cba330ed351603df481cbcdd826f22e40f5
ADNAN MOHAMMED <adnanhani552@gmail.com>
2022-11-29 15:02:13 +0400

    make it better

--- animal/views.py
+++ animal/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User 
 from django.template import Template, Context
 from accounts import models as md
-x= ["أ","ب","ت","ث","ج","ح","خ","د","ذ","ر","ز","س","ش","ص","ض","ط","ع","غ","ف","ق","ك","ل","م","ن","ه","و","ي"]
+x= ["أ","ت","ث","ج","ح","خ","د","ذ","ر","ز","س","ش","ص","ض","ط","ع","غ","ف","ق","ك","ل","م","ن","ه","و","ي"]
 the_letter = 'ب'
 print('this is letterc from views',the_letter)
 
@@ -60,7 +60,7 @@ def room(request,room_code):
     #print('my data is::',t)
     if str(room_code) not in t :
         print('yse is none')
-        models.summeryOfLetter.objects.create(all_letter_as="['أ','ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي']",room=room_code)
+        models.summeryOfLetter.objects.create(all_letter_as="['أ', 'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي']",room=room_code)
         
         return render(request,"game.html", {'username':username , 'room_code' : room_code ,'info':info , 'the_letter':the_letter ,})
 
@@ -74,7 +74,7 @@ def room(request,room_code):
 def room_admin(request,room_code):
 
     info = adnan_test11.objects.filter(room=room_code)
-    models.summeryOfLetter.objects.create(all_letter_as="['أ','ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي']",room=room_code)
+    models.summeryOfLetter.objects.create(all_letter_as="['أ', 'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي']",room=room_code)
     import io   
     import qrcode.image.svg 
     import qrcode
