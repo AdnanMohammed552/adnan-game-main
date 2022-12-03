4bdf6b817505de4a0b089f2cf13814b119af32ee
ADNAN MOHAMMED <adnanhani552@gmail.com>
2022-12-03 15:53:24 +0400

    make it better

--- accounts/views.py
+++ accounts/views.py
@@ -120,7 +120,15 @@ def home(request):
 
 def played(request):
     from .models import played
-    valie = played.objects.all().values()
-    return render (request , 'played.html',{'z':valie})
+    valie = played.objects.all().filter(user=request.user).values()
+    for i in valie:
+        z = i['room_played']
+        break
+    valie2 = models.room_created.objects.all().filter(room_created=z).order_by('-pk').values()
+    for i in valie2:
+        z2 = i['table']
+        break
+    adnan = Template(z2)
 
+    return render (request , 'played.html',{'z':adnan.render(Context({}))})
 
