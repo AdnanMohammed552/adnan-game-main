3dacdcde0b691a8f1bd2cf5263a9579f0267a3b4
ADNAN MOHAMMED <adnanhani552@gmail.com>
2022-12-24 23:16:54 +0400

    make it better

--- animal/admin.py
+++ animal/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from . models import adnan_test11,summeryOfLetter
+from . models import adnan_test11,summeryOfLetter,players
 
 admin.site.register(adnan_test11)
 
 admin.site.register(summeryOfLetter)
+admin.site.register(players)
