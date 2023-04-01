37559b73f7f310ed68f895e947b3efffc3fc9542
AdnanMohammed552 <115033110+AdnanMohammed552@users.noreply.github.com>
2023-04-01 17:59:12 +0400

    make it better

--- animal/admin.py
+++ animal/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from . models import adnan_test11,summeryOfLetter,players
+from . models import adnan_test11,summeryOfLetter,players,room_created
 
 admin.site.register(adnan_test11)
 
 admin.site.register(summeryOfLetter)
 admin.site.register(players)
+admin.site.register(room_created)
