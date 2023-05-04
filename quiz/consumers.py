b48cc9a6314a2b9884038d7d6814ba259f8b0aaa
AdnanMohammed552 <115033110+AdnanMohammed552@users.noreply.github.com>
2023-05-04 23:42:56 +0400

    make it better

--- quiz/consumers.py
+++ quiz/consumers.py
@@ -156,6 +156,7 @@ class gameConsumer(AsyncWebsocketConsumer):
     @sync_to_async
     def data(self,data):
         data = str (data)
+        print('dafqw',data)
         data1 = json.loads(data)
         x = data1[-1]
         data1.pop()
