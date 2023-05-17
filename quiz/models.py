18cee96f3c9ae797c3dfed77b68dc5479b20498c
AdnanMohammed552 <115033110+AdnanMohammed552@users.noreply.github.com>
2023-05-17 23:02:24 +0400

    make it better

--- quiz/models.py
+++ quiz/models.py
@@ -6,6 +6,8 @@ class MyModel(models.Model):
     data = ArrayField(models.CharField(max_length=100), blank=True)
     code = models.IntegerField(max_length=100,unique=True)
     delete = models.BooleanField(default=False)
+    def __str__(self):
+        return self.code
 
 class title(models.Model):
     user =models.CharField(max_length=100,null=True)
@@ -14,11 +16,13 @@ class title(models.Model):
     date = models.DateTimeField(default=timezone.now)
     num = models.CharField(max_length=200,default='')
     delete = models.BooleanField(default=False)
-
+    def __str__(self):
+        return self.title
 class priv(models.Model):
     private = models.BooleanField(default=True)
     code = models.IntegerField(max_length=100,unique=True)
-
+    def __str__(self):
+        return self.code
 
 class room_created(models.Model):
     user = models.CharField(max_length=100)
@@ -26,28 +30,39 @@ class room_created(models.Model):
     players=models.CharField(max_length=1000,null=True,blank=True)
     table=models.TextField(max_length=10000000000000000000,default='')
     date = models.DateTimeField(auto_now_add=True, blank=True)
+    def __str__(self):
+        return self.players
 
 class password(models.Model):
     code =models.CharField(max_length=100,unique=True)
     password = models.CharField(max_length=200)
+    def __str__(self):
+        return self.code
 
 class enumeration(models.Model):
     quiz_number = models.CharField(max_length=10,null=True,blank=True)
     user= models.CharField(max_length=10,unique=True)
+    def __str__(self):
+        return self.user
 class total_score(models.Model):
     total_score = models.CharField(max_length=1000000)
     user= models.CharField(max_length=10,unique=True)
     code =models.CharField(max_length=100,default='')
+    def __str__(self):
+        return self.user
 
 class enumeration_played(models.Model):
     quiz_number_played = models.CharField(max_length=10,null=True,blank=True)
     user= models.CharField(max_length=10,unique=True)
+    def __str__(self):
+        return self.user
 class played_quiz(models.Model):
     user= models.CharField(max_length=100)
     code= models.IntegerField(max_length=1000)
     name = models.CharField(max_length=200,default='')
     ids = models.BigIntegerField(default=0)
-
+    def __str__(self):
+        return self.code
     class Meta:
         unique_together = ('code', 'user')
 
