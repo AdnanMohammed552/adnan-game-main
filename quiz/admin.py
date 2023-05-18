2c20ddab674c72d01d6c4f957d3f372b8196462d
AdnanMohammed552 <115033110+AdnanMohammed552@users.noreply.github.com>
2023-05-18 22:48:58 +0400

    make it better

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import title,MyModel,priv,room_created,password,enumeration,played_quiz
+from .models import title,MyModel,priv,room_created,password,enumeration,played_quiz,total_score,enumeration_played
 admin.site.register(title)
 admin.site.register(MyModel)
 admin.site.register(priv)
 admin.site.register(room_created)
 admin.site.register(password)
 admin.site.register(enumeration)
+admin.site.register(total_score)
+admin.site.register(enumeration_played)
 admin.site.register(played_quiz)
 
 
