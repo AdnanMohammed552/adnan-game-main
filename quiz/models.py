9e3a146c394d5324ad54b7fa5548c39bf1d5f90e
AdnanMohammed552 <115033110+AdnanMohammed552@users.noreply.github.com>
2023-05-19 14:39:32 +0400

    make it better

--- quiz/models.py
+++ quiz/models.py
@@ -17,12 +17,12 @@ class title(models.Model):
     num = models.CharField(max_length=200,default='')
     delete = models.BooleanField(default=False)
     def __str__(self):
-        return self.title
+        return str(self.title)
 class priv(models.Model):
     private = models.BooleanField(default=True)
     code = models.IntegerField(max_length=100,unique=True)
     def __str__(self):
-        return self.code
+        return str(self.code)
 
 class room_created(models.Model):
     user = models.CharField(max_length=100)
@@ -31,38 +31,38 @@ class room_created(models.Model):
     table=models.TextField(max_length=10000000000000000000,default='')
     date = models.DateTimeField(auto_now_add=True, blank=True)
     def __str__(self):
-        return self.players
+        return str(self.players)
 
 class password(models.Model):
     code =models.CharField(max_length=100,unique=True)
     password = models.CharField(max_length=200)
     def __str__(self):
-        return self.code
+        return str(self.code)
 
 class enumeration(models.Model):
     quiz_number = models.CharField(max_length=10,null=True,blank=True)
     user= models.CharField(max_length=10,unique=True)
     def __str__(self):
-        return self.user
+        return str(self.user)
 class total_score(models.Model):
     total_score = models.CharField(max_length=1000000)
     user= models.CharField(max_length=10,unique=True)
     code =models.CharField(max_length=100,default='')
     def __str__(self):
-        return self.user
+        return str(self.user)
 
 class enumeration_played(models.Model):
     quiz_number_played = models.CharField(max_length=10,null=True,blank=True)
     user= models.CharField(max_length=10,unique=True)
     def __str__(self):
-        return self.user
+        return str(self.user)
 class played_quiz(models.Model):
     user= models.CharField(max_length=100)
     code= models.IntegerField(max_length=1000)
     name = models.CharField(max_length=200,default='')
     ids = models.BigIntegerField(default=0)
     def __str__(self):
-        return self.code
+        return str(self.code)
     class Meta:
         unique_together = ('code', 'user')
 
